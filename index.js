18a73fdab7e86f8002bbe54b6c7e6bc723570854
BENHUR LAP <benhuratwork@gmail.com>
2025-03-09 12:55:11 +0530

    new_version8

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const nodemailer = require("nodemailer");
 const app = express();
 const PORT = process.env.PORT || 5000; // Default to 5000 if not specified
 
+console.log("🟢 EMAIL_USER:", process.env.EMAIL_USER);
+console.log("🟡 EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "❌ Not Loaded");
+
+
 app.use(cors());
 app.use(express.json());
 
