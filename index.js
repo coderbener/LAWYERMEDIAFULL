5ebe91b52ac548ebb6b75008f4a03b8bd654c451
BENHUR LAP <benhuratwork@gmail.com>
2025-03-09 10:49:31 +0530

    new_version2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Default to 5000 if not specified
@@ -26,15 +25,6 @@ const services = [
   { id: 5, name: "Banking Law", description: "Covers banking regulations.", image: "/banking.jpg" },
   { id: 6, name: "Family Law", description: "Deals with family-related matters.", image: "/family.jpg" },
 ];
-
-app.get("/3cx/config", async (req, res) => {
-  try {
-      const response = await axios.get("https://1719.3cx.cloud/MyPhone/config/rewitch");
-      res.json(response.data);
-  } catch (error) {
-      res.status(500).json({ error: "Error fetching data from 3CX" });
-  }
-});
 
 // ✅ Route to fetch services
 app.get("/api/services", (req, res) => {
